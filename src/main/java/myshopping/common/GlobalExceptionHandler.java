087fdf1b34db0f80a45ddfcb5d839a70bb219bed
ouhaochen <2571727427@qq.com>
2024-06-30 18:24:50 +0800

    hhh

---
+++ src/main/java/myshopping/common/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package myshopping.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 处理通用异常的方法示例
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+    }
+}
